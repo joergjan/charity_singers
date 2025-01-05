3f81f5ca2799f42c0998d6161d0ae0bbbba100ae
Jan Jörg <jan.joerg.jj@gmail.com>
2025-01-05 10:52:22 +0100

    seo

--- sveltekit-app/src/lib/components/SEO.svelte
+++ sveltekit-app/src/lib/components/SEO.svelte
@@ -5,5 +5,4 @@
 <svelte:head>
 	<title>Charity Singers</title>
 	<meta name="description" content={metaDescription} />
-	<meta property="og_site_name" content="charity-singers.ch" />
 </svelte:head>
